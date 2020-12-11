c44e57d90e27a67652573670a4c292d064e388d6
tars003 <sharmaajay2kuu@gmail.com>
2020-12-11 23:12:13 +0530

    added auth

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const Profile = require("../models/Profile");
+
+module.exports = async function (req, res, next) {
+  const token = req.get("auth-token");
+
+  if(token){
+    try {
+      console.log(token);
+      const { data } = jwt.verify(token, process.env.JWT_SECRET);
+      const id = data.id;
+      let user = await Profile.findById(id);
+      if (user) {
+        req.body.user = user;
+        next();
+      } else{
+        console.log('No user found for the provided token !');
+        return res.status(404).json({
+          sucess: false,
+          message: 'No user found for the provided token !'
+        })
+      }
+    } catch(err) {
+      console.log(err)
+      return res.status(400).json({
+        sucess: false,
+        message: err
+      });
+    }
+
+  } else{
+    console.log('JWT must be provided !');
+    return res.status(400).json({
+      sucess: false,
+      message: 'JWT must be provided !'
+    });
+  }
+
+
+};
--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,79 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 const Profile = require('../models/Profile');
 
+const auth = require('../middleware/auth');
 
 router = Router();
+
+router.get('/token/:no', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({ contactNumber: req.params.no });
+        if(!profile){
+            console.log('No user with found corresponding to given contact number !');
+            return res.status(400).json({
+                sucess: false,
+                message: 'No user with found corresponding to given contact number !'
+            });
+        }
+        const tokenPayload = {
+            data: {
+                id: profile.id
+            }
+        }
+        const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
+        console.log(`Requested token : ${token}`);
+        return res.status(200).json({
+            sucess: true,
+            token,
+        });
+    } catch(err) {
+        console.log(err);
+        return res.status(400).json({
+            sucess: false,
+            message: err
+        })
+    }
+});
+
+router.get('/view', auth, async(req, res) => {
+    try {
+        const profile = await Profile.findOne({ contactNumber: req.body.user.contactNumber });
+        if(!profile){
+            console.log('No profile found for given no. !!');
+            return res.status(200).json({
+                sucess: false,
+                message: 'No profile found for given no. !!'
+            });
+        }
+        console.log('Profile : ');
+        console.log(profile);
+        return res.status(200).json({
+            sucess: true,
+            data: profile
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({
+            sucess: false,
+            message: err
+        });
+    }
+})
 
 router.post('/create', async(req, res) => {
     try {
         const contactNumber = req.body.contactNumber;
+
+        const pro = await Profile.findOne({ contactNumber });
+
+        if (pro){
+            return res.status(200).json({
+                sucess: false,
+                message: 'Profile already exists !'
+            })
+        }
+
         const city = req.body.city;
         const state = req.body.state;
         const occupations = req.body.occupations;
@@ -17,13 +84,21 @@ router.post('/create', async(req, res) => {
             state,
             occupations
         }
+
         console.log(payload);
         console.log(typeof occupations);
         const newProfile = await Profile.create(payload);
 
+        const tokenPayload = {
+            data: {
+              id: newProfile.id
+            },
+        }
+        const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
 
         return res.status(200).json({
             sucess: true,
+            token,
             data: newProfile
         });
 
