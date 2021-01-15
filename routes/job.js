87088aec09963224fe9be114bacbf6d245c5f60c
tars003 <sharmaajay2kuu@gmail.com>
2021-01-15 17:14:38 +0530

    api for home done

--- routes/job.js
+++ routes/job.js
@@ -12,16 +12,19 @@ router.post('/search/profession', auth, async(req, res) => {
     try {
         let obj = req.body;
         obj = JSON.parse(JSON.stringify(obj).replace(/"\s+|\s+"/g, '"'));
-        console.log(typeof obj.professionType[0]);
 
-        const jobs = await Job.find({ professionType: obj.professionType[0] })
-
-        return res.status(200).json({
-            success: true,
-            length: jobs.length,
-            data: jobs
-        });
-
+        Promise.all(obj.professionType.map((i) => {
+          console.log('inside array map');
+          console.log(i);
+          const jobsPromise = Job.find({ professionType: i });
+          return jobsPromise;
+        })).then((result) => {
+          console.log(result);
+          return res.status(200).json({
+              success: true,
+              data: result
+          });
+        })
     } catch(err) {
         console.log(err);
         return res.status(400).json({
