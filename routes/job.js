45f5486923a6dec1cbd88a83935555db44575726
tars003 <sharmaajay2kuu@gmail.com>
2021-01-19 16:17:00 +0530

    already applied filter

--- routes/job.js
+++ routes/job.js
@@ -115,18 +115,50 @@ router.post('/search/profession', auth, async(req, res) => {
         let obj = req.body;
         obj = JSON.parse(JSON.stringify(obj).replace(/"\s+|\s+"/g, '"'));
 
-        Promise.all(obj.professionType.map((i) => {
-          console.log('inside array map');
-          console.log(i);
-          const jobsPromise = Job.find({ professionType: i });
-          return jobsPromise;
-        })).then((result) => {
-          console.log(result);
-          return res.status(200).json({
-              success: true,
-              data: result
-          });
-        })
+        Promise.all(req.body.user.applications.map((application) => {
+            const applicationPromise = Application.findById(application.application);
+            return applicationPromise;
+        })).then((applicationObjArr) => {
+            console.log(applicationObjArr);
+            const jobIds = applicationObjArr.map((applicationObj) => {
+                return applicationObj.job;
+            });
+            console.log('job idssssssssssssssssss');
+            console.log(jobIds);
+            Promise.all(obj.professionType.map((i) => {
+              // console.log('inside array map');
+              // console.log(i);
+              const jobsPromise = Job.find({ professionType: i });
+              return jobsPromise;
+            })).then((result) => {
+                // console.log(result);
+                const jobs = [];
+
+                const jobIdArr = jobIds.map(jobId => {
+                    return String(jobId);
+                })
+
+                result.map((data) => {
+                    const jobData = [];
+                    data.map((job) => {
+                        // Filtering out ALREADY APPLIED JOBS
+                        console.log('444444444444444444444444444444444444444');
+                        console.log(job.id);
+                        console.log(jobIdArr.includes(job.id));
+                        if(!jobIdArr.includes(job.id)){
+                            jobData.push(job);
+                        }
+                    });
+                    jobs.push(jobData);
+                })
+                return res.status(200).json({
+                    success: true,
+                    data: jobs
+                });
+            })
+        });
+
+
     } catch(err) {
         console.log(err);
         return res.status(400).json({
